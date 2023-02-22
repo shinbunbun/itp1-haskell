c55567718837caeffe1c711def870a31836f2597
shinbunbun <takamuneyuto@gmail.com>
2023-02-22 04:01:02 +0000

    fix 6_B

--- 6/6_B.hs
+++ 6/6_B.hs
@@ -4,7 +4,5 @@ import Data.Array
 import Data.List (sort, (\\))
 
 main = do
-  _n <- getLine
   ss <- getContents
-  let res = unlines (((++) <$> ["S ", "H ", "C ", "D "] <*> map show [1 .. 13] :: [String]) \\ lines ss)
-  when (res /= "") $ putStrLn $ init res
+  putStr $ unlines $ ((++) <$> ["S ", "H ", "C ", "D "] <*> map show [1 .. 13] :: [String]) \\ lines ss
